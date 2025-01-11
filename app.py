279f55a886160eacd6e5c696f89fcc43a7cc3771
kim-co-ding <gookae@icloud.com>
2025-01-11 22:10:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,43 @@
-import streamlit as st
-from tensorflow.keras.models import load_model
-from PIL import Image, ImageOps  
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 
-# 파일 업로더를 만듭니다.
-file = st.file_uploader('이미지를 업로드해주세요', type=['jpg','png'])
-
+# Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
-# 모델을 가져옵니다.
+# Load the model
 model = load_model("keras_Model.h5", compile=False)
 
-# 라벨을 읽습니다.
-class_names = open("labels.txt", "r", encoding='UTF-8').readlines()
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
 
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-if file is None:
-  st.text('이미지를 업로드하세요')
-else:   
-    
-    # 파일을 엽니다.
-    image = Image.open(file).convert("RGB")
+# Replace this with the path to your image
+image = Image.open("<IMAGE_PATH>").convert("RGB")
 
-    st.image(image, use_column_width = True)
+# resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
-   
-    image_array = np.asarray(image)  
-    
-    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-   
-    data[0] = normalized_image_array
-    
-    prediction = model.predict(data)
-    index = np.argmax(prediction)
-    # class_name는 1 고양이와 같이 문자열입니다.
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]  
-    confidence_score = round(confidence_score*100, 2)
-    st.text(f'{class_name.split( )[1]} {confidence_score}%')
+# turn the image into a numpy array
+image_array = np.asarray(image)
 
-from tensorflow.keras.models import load_model
-print("Keras is properly imported!")
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+# Predicts the model
+prediction = model.predict(data)
+index = np.argmax(prediction)
+class_name = class_names[index]
+confidence_score = prediction[0][index]
+
+# Print prediction and confidence score
+print("Class:", class_name[2:], end="")
+print("Confidence Score:", confidence_score)
